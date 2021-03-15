b430b028affb36ea1959d8cf3dde56b99829a339
55219 <rodolfo.silva.belo@gmail.com>
2021-03-15 00:39:01 -0300

    login fixes

---
+++ app/src/main/java/com/app/netflixclone/CadastroLogin.kt
@@ -0,0 +1,11 @@
+package com.app.netflixclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_login)
+    }
+}
--- app/src/main/java/com/app/netflixclone/FormLogin.kt
+++ app/src/main/java/com/app/netflixclone/FormLogin.kt
@@ -1,13 +1,44 @@
 package com.app.netflixclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.app.netflixclone.databinding.ActivityFormLoginBinding
 
 class FormLogin : AppCompatActivity() {
+
+    lateinit var binding: ActivityFormLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_login)
+        binding = ActivityFormLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar!!.hide()
+
+        val cadastro = binding.txtTelaCadastro
+        val mensagem = binding.mensagemError
+        val entrar = binding.btEntrar
+
+        entrar.setOnClickListener {
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            if (email.isEmpty() && senha.isEmpty()) {
+                mensagem.setText("Informe o email e senha!")
+            }
+
+        }
+
+
+        cadastro.setOnClickListener {
+
+            val intent = Intent(this, CadastroLogin::class.java)
+            startActivity(intent)
+
+        }
+
+
     }
 }
