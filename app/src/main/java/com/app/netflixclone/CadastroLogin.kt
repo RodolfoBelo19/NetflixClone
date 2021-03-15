33576c4444d249b554f135886db8c6c6bfe415b3
55219 <rodolfo.silva.belo@gmail.com>
2021-03-15 16:15:06 -0300

    login fixes v1.2

--- app/src/main/java/com/app/netflixclone/CadastroLogin.kt
+++ app/src/main/java/com/app/netflixclone/CadastroLogin.kt
@@ -1,11 +1,23 @@
 package com.app.netflixclone
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.netflixclone.databinding.ActivityCadastroLoginBinding
 
 class CadastroLogin : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCadastroLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro_login)
+        binding = ActivityCadastroLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        val toolbar = binding.toolbarCadastro
+        toolbar.setNavigationIcon(R.drawable.ic_netflix_official_logo)
+
     }
 }
